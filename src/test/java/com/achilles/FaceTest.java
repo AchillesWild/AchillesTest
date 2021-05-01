2c381d0f7fc60a62032c6206cb07db7ddced81e9
AchillesWild <532398999@qq.com>
2021-05-01 19:26:51 +0800

    文件处理

--- src/test/java/com/achilles/FaceTest.java
+++ src/test/java/com/achilles/FaceTest.java
@@ -1,4 +1,25 @@
 package com.achilles;
 
+import com.obs.services.model.PutObjectResult;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Base64;
+
 public class FaceTest {
+
+
+    @Test
+    public void upload() throws Exception{
+        String path = "C:\\Users\\Achilles\\Desktop\\1.png";
+        FileInputStream inputStream = new FileInputStream(new File(path));
+//        PutObjectResult result = obsClient.putObject(bucketname, key,inputStream);
+
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+
+        String encoded = Base64.getEncoder().encodeToString(bytes);
+        System.out.println(encoded);
+    }
 }
