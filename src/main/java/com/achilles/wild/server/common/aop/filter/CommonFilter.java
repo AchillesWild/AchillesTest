8fd4ad7188eee3840920b017c6abf93b837cec6d
AchillesWild <532398999@qq.com>
2021-06-23 23:49:13 +0800

    log4j2.xml  ok

--- src/main/java/com/achilles/wild/server/common/aop/filter/CommonFilter.java
+++ src/main/java/com/achilles/wild/server/common/aop/filter/CommonFilter.java
@@ -89,7 +89,7 @@ public class CommonFilter implements Filter {
         log.debug(" ----------- time-consuming : ("+servletPath+")-->("+duration+"ms)");
 
         MDC.remove(CommonConstant.TRACE_ID);
-
+        ThreadContext.remove(CommonConstant.TRACE_ID);
     }
 
     @Override
