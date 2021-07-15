b32172a7998a8d3b75cc64950b08b90eed6f5fc8
AchillesWild <532398999@qq.com>
2021-07-15 23:59:45 +0800

    获取余额  流水

--- src/test/java/com/achilles/wild/server/tool/jdbc/MySqlUtilTest.java
+++ src/test/java/com/achilles/wild/server/tool/jdbc/MySqlUtilTest.java
@@ -9,7 +9,7 @@ public class MySqlUtilTest {
 
 	@Test
 	public void getMapBysqlTest(){
-		List<Map<String,Object>> map = MySqlUtil.getListBysql("select * from citizen order by id desc limit 1");
+		List<Map<String,Object>> map = MySqlUtil.getListBysql("select * from account order by id desc limit 1");
 		System.out.println(map);
 	}
 }
